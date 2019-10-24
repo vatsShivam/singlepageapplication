5fa555595ba5c7a32419000ddc0e705101b1cee1
Shivam <shivamvats0987@gmail.com>
2019-10-24 21:13:56 +0530

    proper indentation of javascript page fixes:#11

--- index.js
+++ index.js
@@ -1,94 +1,73 @@
 (function (){
-var k=document.getElementById('in');
-var s=document.getElementById('heading');
-var input=document.getElementById('register1');
-var input1=document.getElementById('login1');
-var s1=document.getElementById('heading1');
-var lo=document.getElementById('out');
-var reg=document.getElementById('register');
-var log=document.getElementById('login');
-var deletes=document.getElementById('delete');
-var logshows=document.getElementById('logshow');
-lo.addEventListener('click',satyam);
-k.addEventListener('click',shivam);
-/*
-window.onload = function() {
-   shivam();
-   
- };
-
-By default, onload is fired when the entire page loads, including its content (images, CSS, scripts, etc.).
- but The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.,
-*/
- 
- window.addEventListener('DOMContentLoaded', shivam);
-function shivam(){
-    
-   var head=document.createElement('p');
-   head.textContent="Sign in your Account";
-   s.appendChild(head);
-   input1.style.display="none";
-   k.removeEventListener("click", shivam);
-   lo.addEventListener('click',satyam1);  
-   var btn=document.createElement('button');
-    btn.addEventListener('click',local);
-   btn.style.marginTop="70px";
-   btn.style.marginLeft="600px";
-   btn.textContent="Register";
-   reg.appendChild(btn);
-   function satyam1(){
-       s.style.display="none";
-       s1.style.display="inherit";
-       reg.style.display="none";
-       log.style.display="inherit";
-       input1.style.display="inherit";
-       input.style.display="none";
-       
+   var k=document.getElementById('in');
+   var s=document.getElementById('heading');
+   var input=document.getElementById('register1');
+   var input1=document.getElementById('login1');
+   var s1=document.getElementById('heading1');
+   var lo=document.getElementById('out');
+   var reg=document.getElementById('register');
+   var log=document.getElementById('login');
+   var deletes=document.getElementById('delete');
+   var logshows=document.getElementById('logshow');
+   lo.addEventListener('click',satyam);
+   k.addEventListener('click',shivam);
+   window.addEventListener('DOMContentLoaded', shivam);
+   function shivam(){
+      var head=document.createElement('p');
+      head.textContent="Sign in your Account";
+      s.appendChild(head);
+      input1.style.display="none";
+      k.removeEventListener("click", shivam);
+      lo.addEventListener('click',satyam1);  
+      var btn=document.createElement('button');
+      btn.addEventListener('click',local);
+      btn.style.marginTop="70px";
+      btn.style.marginLeft="600px";
+      btn.textContent="Register";
+      reg.appendChild(btn);
+      function satyam1(){
+         s.style.display="none";
+         s1.style.display="inherit";
+         reg.style.display="none";
+         log.style.display="inherit";
+         input1.style.display="inherit";
+         input.style.display="none";
+      }
    }
-}
-function satyam(){
-    var head=document.createElement('p');
-    head.textContent="Login into your account";
-    s.style.display="none";
-    reg.style.display="none";
-    s1.append(head);
-    var but=document.createElement('button');
+   function satyam(){
+      var head=document.createElement('p');
+      head.textContent="Login into your account";
+      s.style.display="none";
+      reg.style.display="none";
+      s1.append(head);
+      var but=document.createElement('button');
       but.style.marginTop="70px";
-       but.style.marginLeft="600px";
+      but.style.marginLeft="600px";
       but.textContent="Login";
       but.addEventListener('click',logged);
-     log.appendChild(but);
-    input1.style.display="inherit";
-    input.style.display="none";
-   
-    lo.removeEventListener("click",satyam);
-    k.addEventListener('click', shivam1);
+      log.appendChild(but);
+      input1.style.display="inherit";
+      input.style.display="none";
+      lo.removeEventListener("click",satyam);
+      k.addEventListener('click', shivam1);
       function shivam1(){
-       s.style.display="inherit";
-       s1.style.display="none";
-       reg.style.display="inherit";
-       log.style.display="none";
-       input.style.display="inherit";
-       input1.style.display="none";
-      
- }  
-          
-         
-   
-}
-
-function local(){
-   var a=document.getElementById('name')
-   var b=document.getElementById('password');
-   localStorage.setItem('name1', a.value);
-   localStorage.setItem('password1',b.value);
-   
-   
-   a.value='';
-   b.value='';
-}
-
-function logged(){
+         s.style.display="inherit";
+         s1.style.display="none";
+         reg.style.display="inherit";
+         log.style.display="none";
+         input.style.display="inherit";
+         input1.style.display="none";
+      }  
+   }
+   function local(){
+      var a=document.getElementById('name')
+      var b=document.getElementById('password');
+      localStorage.setItem('name1', a.value);
+      localStorage.setItem('password1',b.value);
+      a.value='';
+      b.value='';
+   }
+   function logged(){
       var c=localStorage.getItem('name1');
       var d=localStorage.getItem('password1');
       var e=document.getElementById('name2');
@@ -100,16 +79,16 @@ function logged(){
       if(c==g && d==h){
          let stateObj = {
             foo: "bar",
-        };
-        history.pushState(stateObj,"","loggedin");
+         };
+         history.pushState(stateObj,"","loggedin");
          deletes.style.display="none";
          var log4=document.createElement('p');
          log4.textContent="Thanks "+ c +"For logged in ";
          logshows.appendChild(log4);
-               }
+      }
       console.log(g);
       console.log(h);
-     e.value='';
-     f.value='';
+      e.value='';
+      f.value='';
 }
 })();
