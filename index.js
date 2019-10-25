bb6730863ca446e417ec5ee0a1e2da84d0da7fd3
Shivam <shivamvats0987@gmail.com>
2019-10-25 22:08:43 +0530

    var is replaced by const

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 (function (){
-   var k=document.getElementById('in');
-   var s=document.getElementById('heading');
-   var input=document.getElementById('register1');
-   var input1=document.getElementById('login1');
-   var s1=document.getElementById('heading1');
-   var lo=document.getElementById('out');
-   var reg=document.getElementById('register');
-   var log=document.getElementById('login');
-   var deletes=document.getElementById('delete');
-   var logshows=document.getElementById('logshow');
+   const k=document.getElementById('in');
+   const s=document.getElementById('heading');
+   const input=document.getElementById('register1');
+   const input1=document.getElementById('login1');
+   const s1=document.getElementById('heading1');
+   const lo=document.getElementById('out');
+   const reg=document.getElementById('register');
+   const log=document.getElementById('login');
+   const deletes=document.getElementById('delete');
+   const logshows=document.getElementById('logshow');
    lo.addEventListener('click',satyam);
    k.addEventListener('click',shivam);
    window.addEventListener('DOMContentLoaded', shivam);
@@ -34,12 +34,12 @@
       input.style.display="none";
    }
    function satyam(){
-      var head=document.createElement('p');
+      const head=document.createElement('p');
       head.textContent="Login into your account";
       s.style.display="none";
       reg.style.display="none";
       s1.append(head);
-      var but=document.createElement('button');
+      const but=document.createElement('button');
       but.setAttribute("class","loginbtn");
       but.textContent="Login";
       but.addEventListener('click',logged);
@@ -58,17 +58,17 @@
       input1.style.display="none";
    } 
    function local(){
-      var a=document.getElementById('name')
-      var b=document.getElementById('password');
+      const a=document.getElementById('name')
+      const b=document.getElementById('password');
       localStorage.setItem('name1', a.value);
       localStorage.setItem('password1',b.value);
       a.value='';
       b.value='';
    }
    function logged(){
-      var c=localStorage.getItem('name1');
-      var d=localStorage.getItem('password1');
-      var e=document.getElementById('name2');
+      const c=localStorage.getItem('name1');
+      const d=localStorage.getItem('password1');
+      const e=document.getElementById('name2');
       console.log(c);
       console.log(d);
       var f=document.getElementById('password2');
